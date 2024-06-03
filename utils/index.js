324b31f2ea7bb491a8b914d754e9759b8f0c89f1
Alexandre Portella <eualexandre.dev@gmail.com>
2024-06-03 20:20:36 -0300

    adicioado botao de troca de plano de fundo

--- utils/index.js
+++ utils/index.js
@@ -4,3 +4,11 @@ buttonTudum.addEventListener("click", function () {
   const tudum = document.querySelector(".audio");
   tudum.play();
 });
+
+const buttonBg = document.querySelector(".button-bg");
+
+buttonBg.addEventListener("click", () => {
+  const header = document.querySelector(".header");
+
+  header.style.background = "url(../assets/7_Navegando.gif)";
+});
